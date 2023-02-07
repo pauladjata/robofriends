1e8361eef454e216dda27b225c16827463c42ba4
TobyAndTilly <paul.reynolds@adjata.com>
2023-02-07 23:40:58 +1100

    Updates

---
+++ precache-manifest.90059b18e1fc71afa62cfd71f77e86af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed82000aa55ec9b1ae25",
+    "url": "/robofriends/static/css/main.aa54529f.chunk.css"
+  },
+  {
+    "revision": "ed82000aa55ec9b1ae25",
+    "url": "/robofriends/static/js/main.ed82000a.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "654219d9c54512a7130c",
+    "url": "/robofriends/static/css/2.5773ac58.chunk.css"
+  },
+  {
+    "revision": "654219d9c54512a7130c",
+    "url": "/robofriends/static/js/2.654219d9.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0b4b2d658a693f21f60954856d91bdb5",
+    "url": "/robofriends/index.html"
+  }
+];
